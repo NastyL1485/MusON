a02801b803a3f268a78cdec7c8ed75995952619f
NastyL1485 <peretyatkoanastasija@yandex.by>
2024-04-29 00:15:31 +0300

    modified:   src/components/registration/Reg.jsx 	modified:   src/components/registration/reg.css

--- src/components/registration/Reg.jsx
+++ src/components/registration/Reg.jsx
@@ -1,3 +1,4 @@
+import './reg.css';
 import { useEffect, useState } from "react"
 
 
@@ -23,26 +24,26 @@ function Registration() {
 
     const loginHandler = (e) => {
         setLogin(e.target.value)
-        const reLog = /^[a-zA-Z0-9]{1,12}$/
+        const reLog = /^[a-zA-Z0-9]+([a-zA-Z0-9](_|-| )[a-zA-Z0-9])*[a-zA-Z0-9]+$/;
         if (!reLog.test(String(e.target.value).toLowerCase)){
             setLoginError('Логин должен содержать не более 12 символов')
-        } else {setLoginError('')}
+        } else {setLoginError("")}
     }
 
     const emailHandler = (e) => {
         setEmail(e.target.value)
-        const reEmail = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+        const reEmail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
         if (!reEmail.test(String(e.target.value).toLowerCase)){
-            setEmailError('Почта должна быть ликвидна')
-        } else {setEmailError('')}
+            setEmailError('Почта некорректна')
+        } else {setEmailError("")}
     }
 
     const passwordHandler = (e) => {
         setPassword(e.target.value)
-        const rePass = /^(?=.*\d.*\d)[a-zA-Z0-9!@#$%^&*()-_=+;:,.<>/?]+$/;
+        const rePass = /^[@#][A-Za-z0-9]{7,13}$/;
         if (!rePass.test(String(e.target.value).toLowerCase)){
-            setPasswordError('Пароль должен содержать минимум 2 цифры и быть не длиннее 10 символов')
-        } else {setPasswordError('')}
+            setPasswordError('Пароль должен содержать от 7 до 12 символов')
+        } else {setPasswordError("")}
     }
 
     const blurHadler = (e) => {
@@ -65,12 +66,11 @@ function Registration() {
     return (
         <div className="Registration">
             <form>
-                <h1>Регистрация</h1>
-                {(loginDirty && loginError)} && <div className="Error">{loginError}</div>
+                {(loginDirty && loginError) && <div className="Error">{loginError}</div>}
                 <input onChange={e => loginHandler(e)} value={login} onBlur={e => blurHadler(e)} name="login" type="login" placeholder="Логин"></input>
-                {(emailDirty && emailError)} && <div className="Error">{emailError}</div>
+                {(emailDirty && emailError) && <div className="Error">{emailError}</div>}
                 <input onChange={e => emailHandler(e)} value={email} onBlur={e => blurHadler(e)} name="email" type="text" placeholder="Почта"></input>
-                {(passwordDirty && passwordError)} && <div className="Error">{passwordError}</div>
+                {(passwordDirty && passwordError) && <div className="Error">{passwordError}</div>}
                 <input onChange={e => passwordHandler(e)} value={password} onBlur={e => blurHadler(e)} name="password" type="password" placeholder="Пароль"></input>
                 <button disabled={!formValid} type="submit">Создать аккаунт</button>
                 <button>Уже есть аккаунт</button>
